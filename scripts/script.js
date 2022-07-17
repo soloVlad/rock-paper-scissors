f20bfd833abae96bfe328e23b4e0527b53957fee
Vlad Solovey <connectwithvlad@gmail.com>
2022-07-17 16:29:11 +0300

    feat: add game logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const POSSIBLE_CHOICES = ["rock", "paper", "scissors"];
 const ROUND_RESULTS = ["win", "draw", "lose"];  // for player
+const GAME_ROUNDS = 5;
 
 function getComputerChoice() {
     let randomIndex = getRandomInt(POSSIBLE_CHOICES.length);
@@ -15,7 +16,32 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function determineRoundResult(playerChoice, computerChoice) {
+function getRoundResultMessage(playerChoice, computerChoice, roundResult, score) {
+    let playerChoiceCapitalized = capitalizeFirstLetter(playerChoice);
+    let computerChoiceCapitalized = capitalizeFirstLetter(computerChoice);
+    let roundResultMessage = `${score.playerScore}:${score.computerScore}; `;
+
+    switch(roundResult) {
+        case ROUND_RESULTS[0]:
+            roundResultMessage += `You won! ${playerChoiceCapitalized} beats ${computerChoiceCapitalized}`;
+            break;
+        case ROUND_RESULTS[1]:
+            roundResultMessage += `Draw! Computer chose ${playerChoiceCapitalized} too`;
+            break;
+        case ROUND_RESULTS[2]:
+            roundResultMessage += `You lose! ${computerChoiceCapitalized} beats ${playerChoiceCapitalized}`;
+            break;    
+
+    }
+
+    return roundResultMessage; 
+}
+
+function getPlayerChoice() {
+    return prompt("Enter your choice (rock, paper or scissors):").toLowerCase();
+}
+
+function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) return ROUND_RESULTS[1];
 
     switch(playerChoice) {
@@ -31,27 +57,45 @@ function determineRoundResult(playerChoice, computerChoice) {
     }
 }
 
-function getRoundResultMessage(playerChoice, computerChoice, roundResult) {
-    let playerChoiceCapitalized = capitalizeFirstLetter(playerChoice);
-    let computerChoiceCapitalized = capitalizeFirstLetter(computerChoice);
+function updateScore(score, roundResult) {
+    if (roundResult === ROUND_RESULTS[0]) score.playerScore++;
+    else if (roundResult === ROUND_RESULTS[2]) score.computerScore++;
+}
 
-    switch(roundResult) {
-        case ROUND_RESULTS[0]:
-            return `You won! ${playerChoiceCapitalized} beats ${computerChoiceCapitalized}`;
-        case ROUND_RESULTS[1]:
-            return `You lose! ${computerChoiceCapitalized} beats ${playerChoiceCapitalized}`;
-        case ROUND_RESULTS[2]:
-            return `Draw! Computer chose ${playerChoiceCapitalized} too`;
+function getGameResult(score) {
+    let gameResultMessage = `${score.playerScore}:${score.computerScore}; `;
+
+    if (score.playerScore > score.computerScore) {
+        gameResultMessage += "You won!";
+    } else if (score.playerScore < score.computerScore) {
+        gameResultMessage += "You lose(";
+    } else {
+        gameResultMessage += "Draw.";
     }
+
+    return gameResultMessage;
 }
 
-function getPlayerChoice() {
-    return prompt("Enter your choice (rock, paper or scissors):").toLowerCase();
+function game() {
+    let score = {
+        playerScore: 0,
+        computerScore: 0
+    };
+
+    for (let i = 0; i < GAME_ROUNDS; i++) {
+        let playerChoice = getPlayerChoice();
+        console.log(playerChoice);
+        let computerChoice = getComputerChoice();
+
+        let roundResult = playRound(playerChoice, computerChoice);
+        updateScore(score, roundResult);
+        let roundResultMessage = getRoundResultMessage(playerChoice, computerChoice, roundResult, score);
+        console.log(roundResultMessage);
+    }
+
+    let gameResult = getGameResult(score);
+    console.log(gameResult);
 }
 
-function playRound(playerChoice, computerChoice) {
-    let roundResult = determineRoundResult(playerChoice, computerChoice);
-    let roundResultMessage = getRoundResultMessage(playerChoice, computerChoice, roundResult);
-    return roundResultMessage;
-    
-}
+
+game();
